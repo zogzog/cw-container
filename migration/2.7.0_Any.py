d0bae1cf9b86d90b3e399d022a0ff2dafda3801f
Aurelien Campeas <aurelien.campeas@logilab.fr>
2014-09-23 11:25:22 +0200

    Migration: complete with container_etype

--- migration/2.7.0_Any.py
+++ migration/2.7.0_Any.py
@@ -1,5 +1,7 @@
-from cubes.container import config
+from cubes.container import config as cconfig
 
-for etype in config.Container.all_etypes():
-    conf = config.by_etype(etype)
+for etype in cconfig.Container.all_etypes():
+    conf = cconfig.Container.by_etype(etype)
     add_relation_definition(etype, conf.crtype, etype)
+    add_relation_definition(etype, 'container_etype', 'CWEType')
+
