4fad91dc585d75bce3f317aeae877a2c8d822fc2
Philippe Pepiot <philippe.pepiot@logilab.fr>
2017-11-15 16:25:17 +0100

    [hooks] Use Connection instead of Session
    Session was dropped in CubicWeb 3.25

--- hooks.py
+++ hooks.py
@@ -22,7 +22,7 @@ from logilab.common.registry import Predicate
 
 from cubicweb import ValidationError, onevent
 from cubicweb.server.hook import Hook, DataOperationMixIn, Operation
-from cubicweb.server.session import Session
+from cubicweb.server.session import Connection
 
 from cubes.container.utils import parent_rschemas
 from cubes.container.config import Container, clear_callback
@@ -235,7 +235,7 @@ class CloneContainer(Hook):
 
 class CloneContainerOp(DataOperationMixIn, Operation):
 
-    def prepare_cloned_container(self, session, clone):
+    def prepare_cloned_container(self, cnx, clone):
         """ give a chance to cleanup cloned container before the process starts
         e.g.: it may already have a workflow state but we want to ensure it has none
         before it is entirely cloned
@@ -244,12 +244,11 @@ class CloneContainerOp(DataOperationMixIn, Operation):
 
     def postcommit_event(self):
         for cloneid in self.get_data():
-            session = Session(self.cnx.user, self.cnx.repo)
-            with session.new_cnx() as cnx:
+            with Connection(self.cnx.repo, self.cnx.user) as cnx:
                 cloned = cnx.entity_from_eid(cloneid)
                 config = Container.by_etype(cloned.cw_etype)
                 with cnx.deny_all_hooks_but(*config.compulsory_hooks_categories):
-                    self.prepare_cloned_container(session, cloned)
+                    self.prepare_cloned_container(cnx, cloned)
                     cloned.cw_adapt_to('Container.clone').clone()
                     self.finalize_cloned_container(cnx, cloned)
                     cnx.commit()
